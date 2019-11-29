5027afab65772d9e6216d474b866ac5a94863d3b
Reyoung123 <reyoung63@gmail.com>
2019-11-29 11:40:45 +0545

    Initial commit

---
+++ app/src/main/java/com/kishor/cameraapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.kishor.cameraapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    protected ImageView imageview;
+private Button btn_image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageview=findViewById(R.id.img);
+        btn_image = findViewById(R.id.btn1);
+        checkPermission();
+btn_image.setOnClickListener (new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        loadCamera();
+    }
+});
+    }
+
+    private void loadCamera() {
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 0);
+    }
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0 );
+
+    }
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==0 && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap =(Bitmap) extras.get("data");
+            imageview.setImageBitmap(imageBitmap);
+
+        }
+    }}
